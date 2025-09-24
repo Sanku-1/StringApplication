721fc7e1c3983cf2b4151e795649ca2fb28fcd64
Stephen <ankustephen99@gmail.com>
2025-09-24 13:45:07 -0400

    Added answers

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -14,10 +14,22 @@ public class VariableApp {
         hasPets = false;
         niceMessage = "Don't give up!";
 
-        System.out.println("1. The answer is " + favoriteColor);
-        System.out.println("2. The answer is " + yearStarted);
-        System.out.println("3. The answer is " + middleInitial);
-        System.out.println("4. The answer is " + hasPets);
-        System.out.println("5. The answer is " + niceMessage);
+        System.out.println("STEP 1");
+        System.out.println("1. My favorite color is " + favoriteColor);
+        System.out.println("2. The year I started this class is " + yearStarted);
+        System.out.println("3. My middle initial is " + middleInitial);
+        System.out.println("4. Me owning pets is " + hasPets);
+        System.out.println("5. A nice message is: " + niceMessage);
+
+        byte daysInWeek = 7;
+        String priceCoffee = "$4.99";
+        char favoriteLetter = 'S';
+        boolean isRaining = false;
+
+        System.out.println("STEP 2");
+        System.out.println("There are " + daysInWeek + " days in a week");
+        System.out.println("The price of coffee is " + priceCoffee);
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("The assertion that it is raining is " + isRaining);
     }
 }
