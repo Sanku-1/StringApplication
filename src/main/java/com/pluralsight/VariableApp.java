870b7e6aa35992af094fafd15febb7d2475c9b0f
Stephen <ankustephen99@gmail.com>
2025-09-24 13:36:47 -0400

    Added answers

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,10 +2,22 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
-        String favoriteColor = "Pink";
-        int yearStarted = 2025;
-        char middleInitial = 'E';
-        boolean hasPets = false;
-        String niceMessage = "Time and tide wait for no man";
+        String favoriteColor;
+        int yearStarted;
+        char middleInitial;
+        boolean hasPets;
+        String niceMessage;
+
+        favoriteColor = "pink";
+        yearStarted = 2025;
+        middleInitial = 'E';
+        hasPets = false;
+        niceMessage = "Don't give up!";
+
+        System.out.println("1. The answer is " + favoriteColor);
+        System.out.println("2. The answer is " + yearStarted);
+        System.out.println("3. The answer is " + middleInitial);
+        System.out.println("4. The answer is " + hasPets);
+        System.out.println("5. The answer is " + niceMessage);
     }
 }
