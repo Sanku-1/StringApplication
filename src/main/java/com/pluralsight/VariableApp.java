158c14d47cc3421dee9d73ae2844cc0067358263
Stephen <ankustephen99@gmail.com>
2025-09-24 13:31:08 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String favoriteColor = "Pink";
+        int yearStarted = 2025;
+        char middleInitial = 'E';
+        boolean hasPets = false;
+        String niceMessage = "Time and tide wait for no man";
+    }
+}
